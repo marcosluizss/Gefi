dad64183bc3a96d73f15d690ee8a95cc9879232b
Marcos Luiz <marcosluizss@gmail.com>
2014-08-17 23:03:01 -0300

    Adiciona novo layout de menu com imagens e itens

--- app/src/main/java/br/com/ml2s/gefi/Menu.java
+++ app/src/main/java/br/com/ml2s/gefi/Menu.java
@@ -1,5 +1,6 @@
 package br.com.ml2s.gefi;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
@@ -7,7 +8,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by marcossantos on 15/08/2014.
@@ -18,8 +24,29 @@ public class Menu extends ListFragment {
         return new Menu();
     }
 
-    private String[] arrMenu;
-    private ArrayAdapter<String> ArrayAdapter;
+    // Array of strings storing country names
+    String[] txtItem = new String[] {
+            "Bancos",
+            "Contas",
+            "Cartões",
+            "Movimentações"
+    };
+
+    // Array of integers points to images stored in /res/drawable/
+    int[] img = new int[]{
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+            R.drawable.ic_launcher,
+    };
+
+    // Array of strings to store currencies
+    String[] qtdOcorrencias = new String[]{
+            "1",
+            "3",
+            "7",
+            "0",
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,9 +62,24 @@ public class Menu extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        arrMenu = getResources().getStringArray(R.array.menu_principal);
-        ArrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,arrMenu);
-        setListAdapter(ArrayAdapter);
+        Resources resources = getResources();
+
+        List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
+
+        for (int i=0;i<txtItem.length;i++){
+            HashMap<String,String> hm = new HashMap<String, String>();
+            hm.put("txt",txtItem[i]);
+            hm.put("qtd",qtdOcorrencias[i]);
+            hm.put("ico",Integer.toString(img[i]));
+
+            aList.add(hm);
+        }
+
+        String[] from = {"ico","txt","qtd"};
+        int[] to = {R.id.iv_menu_item,R.id.tv_menu_texto,R.id.tv_menu_ocorrencias};
+
+        SimpleAdapter adapter = new SimpleAdapter(getActivity().getBaseContext(), aList, R.layout.menu_item, from, to);
+        setListAdapter(adapter);
     }
 
     @Override
